2a07d894e9b2d500865b7eeb3382b30651d4597e
Agustin Leitao <agleitao@deloitte.com>
2017-08-05 20:58:11 -0300

    Initial commit

---
+++ NaVR/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+    private const float TILE_SIZE = 1.0F;
+    private const float TILE_OFFSET = 0.5F;
+    
+    private int selectionX = -1;
+    private int selectionY = -1;
+
+    private Vector3 widthLine;
+    private Vector3 heightLine;
+
+    public GameObject reticle;
+
+    void Start ()
+    {       
+        widthLine = Vector3.right * 10;
+        heightLine = Vector3.forward * 10;     
+    }
+
+    void Update() {
+       
+        Debug.Log("Reticle. X: " + selectionX + " Y: " + selectionY);
+        UpdateSelection();
+        DrawBoard();
+	}
+
+    private void DrawBoard()
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            Vector3 start = Vector3.right * i;
+            Debug.DrawLine(start, start + heightLine);
+        }
+
+        for (int j = 0; j <= 10; j++)
+        {
+            Vector3 start = Vector3.forward * j;
+            Debug.DrawLine(start, start + widthLine);
+        }
+    }
+
+    private void UpdateSelection()
+    {
+        if (!Camera.main)
+            return;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(new Ray(reticle.transform.position, reticle.transform.forward), out hit, 25f, LayerMask.GetMask("ChessBoard")))
+        {
+            selectionX = (int)hit.point.x;
+            selectionY = (int)hit.point.z;                 
+        }
+        else
+        {
+            selectionX = -1;
+            selectionY = -1;
+        }
+    }
+}
---
+++ NaVR/Assets/Scripts/MoveUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BoxUp()
+    {
+        transform.position += new Vector3(0f, 1f, 0f);
+    }
+}
